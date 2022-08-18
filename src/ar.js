b959eb5a21c405d3c8cdaa6fd15d5ba1bc503f6d
YamaD <me@dyama.net>
2022-08-18 19:11:18 +0900

    wip

--- src/ar.js
+++ src/ar.js
@@ -1,6 +1,8 @@
 import "mind-ar/dist/mindar-image.prod.js"
 import "aframe"
-import "aframe-extras"
+// import "aframe-extras"
 import "mind-ar/dist/mindar-image-aframe.prod.js"
+
+// a-うんたらにanimation-mixerをつけるとモデルのアニメーションが見られる
 
 window.handler = led => {}
